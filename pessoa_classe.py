3975edc86d0a0818349b6c2f50cf2107a7da67f0
AnnaAngelina <annaangeh169@gmail.com>
2024-12-27 16:36:38 -0300

    criação das classes pessoas

---
+++ pessoa_classe.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+class Pessoa(ABC):
+    def __init__(self, nome, cpf, dataNascimento, email, senha):
+        self.nome = nome
+        self._cpf = cpf
+        self.dataNascimento = dataNascimento
+        self._email = email
+        self.__senha = senha
+    def __getsenha(self):
+        return self.__senha
+    def mudarsenha(self):
+        esc = input('Digite sua senha antiga (caso não saiba digite 1 para mudar pelo CPF): ')
+        if esc != '1':
+            senha_antiga = esc
+            if senha_antiga == self.__getsenha():
+                while True:
+                    nova_senha = input('Digite sua nova senha: ')
+                    if nova_senha != senha_antiga:
+                        self.__senha = nova_senha
+                        print('Senha alterada com sucesso!')
+                        break
+                    else:
+                        print('Digite novamente uma senha que não seja a mesma que sua antiga!')
+            else:
+                print('Senha incorreta')
+        else:
+            cpf_v = input('Digite seu CPF: ')
+            if cpf_v == self._cpf:
+                nova_senha = input('Digite sua nova senha: ')
+                self.__senha = nova_senha
+                print('Senha alterada com sucesso!')
+            else:
+                print('CPF inválido!')
+    @abstractmethod
+    def falar(self):
+        pass
+        
+        
+    
---
+++ visitante_classe.py
@@ -0,0 +1,10 @@
+from pessoa_classe import Pessoa
+class Visitante(Pessoa):
+    def __init__(self, nome, cpf, dataNascimento, email, senha):
+        super().__init__(nome, cpf, dataNascimento, email, senha)
+    def falar(self):
+        print(f'Olá, me chamo {self.nome.title()} vim para conhecer mais sobre dinossauros!')
+    def acessar(self):
+        item = input(f'Desejo acessar o item: ')
+        return item
+
